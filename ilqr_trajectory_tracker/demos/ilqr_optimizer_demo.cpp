063190e2be41240d5b69c8dc47661d13713f1403
Cihat Kurtuluş Altıparmak <cihataltiparmak1@gmail.com>
2025-03-05 22:30:31 +0300

    Fixed the bug of assigning ilqr optimization start state (#39)

--- ilqr_trajectory_tracker/demos/ilqr_optimizer_demo.cpp
+++ ilqr_trajectory_tracker/demos/ilqr_optimizer_demo.cpp
@@ -56,7 +56,9 @@ int main(int argc, char ** argv)
   newton_optimizer.setAlpha(alpha);
 
   const auto start{std::chrono::steady_clock::now()};
-  auto u_optimal = newton_optimizer.optimize(x_feasible, Q, R, dt);
+  // TODO: Fix demo later, we should pass the actual robot pose instead of
+  // the beginning pose of the feasible trajectory
+  auto u_optimal = newton_optimizer.optimize(x_feasible[0], x_feasible, Q, R, dt);
   const auto end{std::chrono::steady_clock::now()};
 
   const std::chrono::duration<double> elapsed_seconds{end - start};
--- nav2_frenet_ilqr_controller/src/frenet_ilqr_controller.cpp
+++ nav2_frenet_ilqr_controller/src/frenet_ilqr_controller.cpp
@@ -206,7 +206,7 @@ nav_msgs::msg::Path FrenetILQRController::truncateGlobalPlanWithLookAheadDist(
 }
 
 Vector2d FrenetILQRController::findOptimalInputForTrajectory(
-  const geometry_msgs::msg::PoseStamped & /*robot_pose*/,
+  const Vector3d & c_state_robot,
   const frenet_trajectory_planner::CartesianTrajectory & robot_cartesian_trajectory)
 {
 
@@ -230,7 +230,7 @@ Vector2d FrenetILQRController::findOptimalInputForTrajectory(
   ilqr_trajectory_tracker::NewtonOptimizer<DiffDriveRobotModel> newton_optimizer;
   newton_optimizer.setIterationNumber(20);
   newton_optimizer.setAlpha(alpha);
-  auto U_optimal = newton_optimizer.optimize(X_feasible, Q, R, dt);
+  auto U_optimal = newton_optimizer.optimize(c_state_robot, X_feasible, Q, R, dt);
 
   if (U_optimal.empty()) {
     throw nav2_core::NoValidControl("Iterative LQR couldn't find any solution!");
@@ -301,13 +301,16 @@ geometry_msgs::msg::TwistStamped FrenetILQRController::computeVelocityCommands(
     transformed_plan.header.frame_id,
     planned_cartesian_trajectory);
   frenet_plan = path_handler_->transformPath(costmap_ros_->getBaseFrameID(), frenet_plan);
-
   planned_cartesian_trajectory = convertToCartesianTrajectory(frenet_plan);
 
   truncated_path_pub_->publish(frenet_plan);
   robot_pose_pub_->publish(robot_pose);
 
-  auto u_opt = findOptimalInputForTrajectory(robot_pose, planned_cartesian_trajectory);
+  path_handler_->transformPose(costmap_ros_->getBaseFrameID(), robot_pose, robot_pose);
+  Vector3d c_state_robot;
+  c_state_robot << robot_pose.pose.position.x, robot_pose.pose.position.y,
+    tf2::getYaw(robot_pose.pose.orientation);
+  auto u_opt = findOptimalInputForTrajectory(c_state_robot, planned_cartesian_trajectory);
 
   // populate and return message
   geometry_msgs::msg::TwistStamped cmd_vel;
