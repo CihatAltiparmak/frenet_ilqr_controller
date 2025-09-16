724aeca8030c1dcfb7d5a5c56e5b5d2bcd74937d
Cihat Kurtuluş Altıparmak <cihataltiparmak1@gmail.com>
2025-09-16 21:12:27 +0300

    Keep updated with nav2_rolling by removing unnecessary headers (#48)
    Signed-off-by: CihatAltiparmak <cihataltiparmak1@gmail.com>
    (cherry picked from commit 30536b43dc5d7a01ecd2095017a32c0930dd7f1c)
    
    # Conflicts:
    #	nav2_frenet_ilqr_controller/include/nav2_frenet_ilqr_controller/path_handler.hpp

--- nav2_frenet_ilqr_controller/include/nav2_frenet_ilqr_controller/path_handler.hpp
+++ nav2_frenet_ilqr_controller/include/nav2_frenet_ilqr_controller/path_handler.hpp
@@ -26,8 +26,12 @@
 #include "nav2_costmap_2d/footprint_collision_checker.hpp"
 #include "nav2_util/odometry_utils.hpp"
 #include "nav2_util/geometry_utils.hpp"
+<<<<<<< HEAD
 #include "nav2_core/exceptions.hpp"
 #include "geometry_msgs/msg/pose2_d.hpp"
+=======
+#include "nav2_core/controller_exceptions.hpp"
+>>>>>>> 30536b4 (Keep updated with nav2_rolling by removing unnecessary headers (#48))
 
 namespace nav2_frenet_ilqr_controller
 {
