dbbecabf450250c25d406f131ae9143f0462619c
Cihat Kurtuluş Altıparmak <cihataltiparmak1@gmail.com>
2026-06-24 23:50:31 +0300

    Add constraints policy to eliminate trajectories that does not obey (#79)
    constraints, fix base policy class compilation bug and add forgotten doc
    about LatLonBalanceCost

--- nav2_frenet_ilqr_controller/include/nav2_frenet_ilqr_controller/policies/constraints_policy.hpp
+++ nav2_frenet_ilqr_controller/include/nav2_frenet_ilqr_controller/policies/constraints_policy.hpp
@@ -15,50 +15,38 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NAV2_FRENET_ILQR_CONTROLLER__POLICIES__RCLCPP_NODE_POLICY_HPP_
-#define NAV2_FRENET_ILQR_CONTROLLER__POLICIES__RCLCPP_NODE_POLICY_HPP_
+#ifndef NAV2_FRENET_ILQR_CONTROLLER__POLICIES__CONSTRAINTS_POLICY_HPP_
+#define NAV2_FRENET_ILQR_CONTROLLER__POLICIES__CONSTRAINTS_POLICY_HPP_
 
 #include <memory>
 #include <string>
-#include "frenet_trajectory_planner/policies/base_policy.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "nav2_costmap_2d/costmap_2d_ros.hpp"
-
-using namespace frenet_trajectory_planner::policies;  // NOLINT
-using namespace frenet_trajectory_planner;  // NOLINT
+#include "nav2_frenet_ilqr_controller/policies/rclcpp_node_policy.hpp"
 
 namespace nav2_frenet_ilqr_controller
 {
 namespace policies
 {
 
-class RclcppNodePolicy : public Policy
+class ConstraintsPolicy : public RclcppNodePolicy
 {
 public:
-  RclcppNodePolicy()
-  : Policy() {}
-  virtual void initialize(
+  ConstraintsPolicy();
+  void initialize(
     const std::string & policy_plugin_name,
     const nav2::LifecycleNode::WeakPtr & parent,
-    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
-  {
-    policy_plugin_name_ = policy_plugin_name;
-    node_ = parent;
-    costmap_ros_ = costmap_ros;
-    costmap_ = costmap_ros_->getCostmap();
-  }
-  virtual bool checkIfFeasible(
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+  bool checkIfFeasible(
     const FrenetTrajectory & frenet_trajectory,
-    const CartesianTrajectory & cartesian_trajectory) = 0;
+    const CartesianTrajectory & cartesian_trajectory) override;
 
-protected:
-  nav2::LifecycleNode::WeakPtr node_;
-  std::string policy_plugin_name_;
-  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
-  nav2_costmap_2d::Costmap2D * costmap_{nullptr};
+  bool isCollides(const CartesianState & cartesian_state);
+
+private:
+  double max_speed_;
+  double max_acceleration_;
 };
 
 }  // namespace policies
 }  // namespace nav2_frenet_ilqr_controller
 
-#endif  // NAV2_FRENET_ILQR_CONTROLLER__POLICIES__RCLCPP_NODE_POLICY_HPP_
+#endif  // NAV2_FRENET_ILQR_CONTROLLER__POLICIES__CONSTRAINTS_POLICY_HPP_
